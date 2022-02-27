73ed211f1bc31f3447f9c094e5d65f2b1663e8bc
Devil7DK <dineshthangavel47@gmail.com>
2022-02-27 16:02:22 +0530

    chore: Add sample

---
+++ samples/basic.ts
@@ -0,0 +1,9 @@
+import { Lexer, Tagger, TagType } from '@devil7softwares/pos';
+
+const lexer = new Lexer();
+const tagger = new Tagger();
+
+const words = lexer.lex('This is some sample text. This text can contain multiple sentences.');
+const taggedWords = tagger.tag(words);
+
+console.table(taggedWords);
---
+++ samples/extend-lexicon.ts
@@ -0,0 +1,9 @@
+import { Lexer, Tagger, TagType } from '@devil7softwares/pos';
+
+const tagger = new Tagger();
+
+tagger.extendLexicon({ Obama: [TagType.NNP] });
+
+const taggedWords = tagger.tag(['Mr', 'Obama']); // --> [[ 'Mr', 'NNP' ], [ 'Obama', 'NNP' ]]
+
+console.table(taggedWords);
